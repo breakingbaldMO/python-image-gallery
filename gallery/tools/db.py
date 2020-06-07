c074f688cdffb8efc478a6c6742bf1ad1b37c0f4
breakingbaldMO <33108238+breakingbaldMO@users.noreply.github.com>
2020-06-07 14:00:52 -0500

    restructured project folders: created user_admin.py for menu command line UI program, separate db.py for CRUD methods

--- gallery/tools/db.py
+++ gallery/tools/db.py
@@ -19,6 +19,7 @@ def get_password():
 def connect():
     global connection
     connection = psycopg2.connect(host=db_host, dbname=db_name, user=db_user, password=get_password())
+    connection.set_session(autocommit=True)
 
 
 def execute(query, args=None):
@@ -37,7 +38,7 @@ def add_user(username, password, full_name):
         execute("""
         INSERT into users (username, password, full_name) VALUES (%s, %s, %s);
         """, (username, password, full_name))
-        connection.commit()
+
 
     except Exception as error:
         print("Error: a user with username '" + username + "' already exists\n")
@@ -47,14 +48,14 @@ def edit_user(user_to_edit, password, full_name):
     try:
         if password:
                 execute("UPDATE users SET password='" + password + "' WHERE username='" + user_to_edit + "';")
-                connection.commit()
+
 
     except Exception as error:
                 print("Error updating password\n")
     try:
         if full_name:
                 execute("UPDATE users SET full_name='" + full_name + "' WHERE username='" + user_to_edit + "';")
-                connection.commit()
+
 
     except Exception as error:
                 print("Error updating full name\n")
@@ -63,7 +64,7 @@ def edit_user(user_to_edit, password, full_name):
 def delete_user(user_to_delete):
     try:
         execute("DELETE FROM users WHERE username='" + user_to_delete + "';")
-        connection.commit()
+
     except Exception as error:
             print("Error deleting username\n")
 
@@ -71,81 +72,3 @@ def delete_user(user_to_delete):
 def select_all(table):
     res = execute('select * from ' + table).fetchall()
     return res
-
-
-def menu():
-    choice = input("1) List users\n2) Add user\n3) Edit user\n4) Delete user\n5) Quit\nEnter Command> ")
-    choice = int(choice)
-    if choice == 1:
-        print("\nList Users\n")
-        res = select_all("users")
-        print("username     password     full name\n------------------------------------")
-        for row in res:
-            formatted = str(row).strip('(),\'')
-            formatted = formatted.replace("\'", "")
-            formatted = formatted.replace(",", "        ")
-            print(formatted)
-        print("\n")
-        menu()
-
-    elif choice == 2:
-        print("\nAdd User\n")
-        username = input("Username>")
-        password = input("Password>")
-        full_name = input("Full name>")
-        if not username:
-            print("username must be specified\n")
-            menu()
-        else:
-            add_user(username, password, full_name)
-            menu()
-
-    elif choice == 3:
-        print("\nEdit User\n")
-        user_to_edit = input("\nUsername to edit>")
-        cursor = connection.cursor()
-        cursor.execute("select * from users where username='" + user_to_edit + "';")
-        res = cursor.fetchall()
-        if not res:
-            print("\nNo such user exists\n")
-            menu()
-        else:
-            password = input("New password (press enter to keep current)>")
-            full_name = input("New full name (press enter to keep current)>")
-            edit_user(user_to_edit, password, full_name)
-            menu()
-
-    elif choice == 4:
-        print("\nDelete User\n")
-        user_to_delete = input("\nEnter username to delete>")
-        cursor = connection.cursor()
-        cursor.execute("select * from users where username='" + user_to_delete + "';")
-        res = cursor.fetchall()
-        if not res:
-            print("\nNo such user exists\n")
-            menu()
-        else:
-            answer = input("\nAre you sure that you want to delete " + user_to_delete + " ?")
-            if answer is "Yes" or "Y":
-                delete_user(user_to_delete)
-                menu()
-
-    elif choice == 5:
-        print("\nGoodbye!\n")
-
-
-def main():
-    connect()
-    menu()
-
-    # res = execute('select * from users')
-    # for row in res:
-    #    print(row)
-    # res = execute("update users set password=%s where username='fred'", ('banana',))
-    # res = execute('select * from users')
-    # for row in res:
-    #    print(row)
-
-
-if __name__ == '__main__':
-    main()
--- gallery/tools/user_admin.py
+++ gallery/tools/user_admin.py
@@ -1,77 +1,4 @@
-import psycopg2
-
-db_host = "database-1.cv1n9oljqdta.us-east-1.rds.amazonaws.com"
-db_name = "image_gallery"
-db_user = "image_gallery"
-
-password_file = "/home/ec2-user/.image_gallery_config"
-
-connection = None
-
-
-def get_password():
-    f = open(password_file, "r")
-    result = f.readline()
-    f.close()
-    return result[:-1]
-
-
-def connect():
-    global connection
-    connection = psycopg2.connect(host=db_host, dbname=db_name, user=db_user, password=get_password())
-    connection.set_session(autocommit=True)
-
-
-def execute(query, args=None):
-    global connection
-    cursor = connection.cursor()
-    if not args:
-        cursor.execute(query)
-    else:
-        cursor.execute(query, args)
-    return cursor
-
-
-def add_user(username, password, full_name):
-    cursor = connection.cursor()
-    try:
-        execute("""
-        INSERT into users (username, password, full_name) VALUES (%s, %s, %s);
-        """, (username, password, full_name))
-
-
-    except Exception as error:
-        print("Error: a user with username '" + username + "' already exists\n")
-
-
-def edit_user(user_to_edit, password, full_name):
-    try:
-        if password:
-                execute("UPDATE users SET password='" + password + "' WHERE username='" + user_to_edit + "';")
-
-
-    except Exception as error:
-                print("Error updating password\n")
-    try:
-        if full_name:
-                execute("UPDATE users SET full_name='" + full_name + "' WHERE username='" + user_to_edit + "';")
-
-
-    except Exception as error:
-                print("Error updating full name\n")
-
-
-def delete_user(user_to_delete):
-    try:
-        execute("DELETE FROM users WHERE username='" + user_to_delete + "';")
-
-    except Exception as error:
-            print("Error deleting username\n")
-
-
-def select_all(table):
-    res = execute('select * from ' + table).fetchall()
-    return res
+from gallery.tools.db import add_user, select_all, connection, edit_user, delete_user, connect
 
 
 def menu():
@@ -80,7 +7,7 @@ def menu():
     if choice == 1:
         print("\nList Users\n")
         res = select_all("users")
-        print("username                   password                 full name\n---------------------------------------------------------------------")
+        print("username                  password                  full name\n---------------------------------------------------------------------")
         for row in res:
            print("{: <25} {: <25} {: <25}".format(*row))
         print("\n")
@@ -92,7 +19,7 @@ def menu():
         password = input("Password>")
         full_name = input("Full name>")
         if not username:
-            print("username must be specified\n")
+            print("Failed to add user: username must be specified\n")
             menu()
         else:
             add_user(username, password, full_name)
