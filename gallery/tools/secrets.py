6d02f8d90e5209d3baddec099bcbcf67cb77a078
breakingbaldMO <33108238+breakingbaldMO@users.noreply.github.com>
2020-06-12 08:40:01 -0500

    updated secrets.py to debug possible error

--- gallery/tools/secrets.py
+++ gallery/tools/secrets.py
@@ -7,9 +7,8 @@ import base64
 from botocore.exceptions import ClientError
 
 
-def get_secret_image_gallery():
-
-    secret_name = "sec-ig-image_gallery"
+def get_secret():
+    secret_name = "sec-ig-postgres"
     region_name = "us-east-1"
 
     # Create a Secrets Manager client
@@ -31,27 +30,22 @@ def get_secret_image_gallery():
         if e.response['Error']['Code'] == 'DecryptionFailureException':
             # Secrets Manager can't decrypt the protected secret text using the provided KMS key.
             # Deal with the exception here, and/or rethrow at your discretion.
-            print('DecryptionFailureException')
             raise e
         elif e.response['Error']['Code'] == 'InternalServiceErrorException':
             # An error occurred on the server side.
             # Deal with the exception here, and/or rethrow at your discretion.
-            print('InternalServiceErrorException')
             raise e
         elif e.response['Error']['Code'] == 'InvalidParameterException':
             # You provided an invalid value for a parameter.
             # Deal with the exception here, and/or rethrow at your discretion.
-            print('InvalidParameterException')
             raise e
         elif e.response['Error']['Code'] == 'InvalidRequestException':
             # You provided a parameter value that is not valid for the current state of the resource.
             # Deal with the exception here, and/or rethrow at your discretion.
-            print('InvalidRequestException')
             raise e
         elif e.response['Error']['Code'] == 'ResourceNotFoundException':
             # We can't find the resource that you asked for.
             # Deal with the exception here, and/or rethrow at your discretion.
-            print('ResourceNotFoundException')
             raise e
     else:
         # Decrypts secret using the associated KMS CMK.
@@ -61,7 +55,5 @@ def get_secret_image_gallery():
         else:
             decoded_binary_secret = base64.b64decode(get_secret_value_response['SecretBinary'])
 
-    return secret
-
+        return secret
     # Your code goes here.
-
