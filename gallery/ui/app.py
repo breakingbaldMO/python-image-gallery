cbd4a4091fbaac922d727fd397cf14aa8ccfe769
EC2 Default User <ec2-user@ip-10-0-1-72.ec2.internal>
2020-06-15 02:28:57 +0000

    Added ui folder and web app programs (Flask)

--- gallery/ui/app.py
+++ gallery/ui/app.py
@@ -1,25 +1,63 @@
 from flask import Flask
 from flask import request
 from flask import render_template
-from gallery/tools/db import add_user
+from flask import redirect, url_for
+import db
+import secrets
+
 
 app = Flask(__name__)
 
-@app.route('/admin')
-def hello_world():
-    return 'Welcome to the "users" database table!'
 
-@app.route('/addUser')
-def goodbye():
-    return 'addUser'
+@app.route('/admin', methods=["GET", "POST"])
+def index():
+    db.connect()
+        
+    return render_template('index.html', username=db.select_all_usernames("users"))
 
-@app.route('/greet/<name>')
-def greet(name):
-    return 'Nice to meet you ' + name
+@app.route('/admin/edit/<username>')
+def edit(username):
+     return render_template('edit.html', username=username)
 
-@app.route('/mult')
-def mult():
-    x = request.args['x']
-    y = request.args['y']
-    return 'the product is ' + str(int(x) * int(y))
+@app.route('/admin/edit/modify', methods=['POST'])
+def modify():
+    db.connect()
+    username = request.form['username']
+    password = request.form['password']
+    full_name = request.form['full_name']
+    db.edit_user(username, password, full_name)
+    res = db.select_user_info(username, 'users') 
+    db.close()
+    return render_template('modify.html', user_info=res)
+    
+@app.route('/admin/addUser')
+def addUser():
+    return render_template('addUser.html')
 
+@app.route('/admin/addUser/added', methods=['POST'])
+def added():
+    db.connect()
+    username = request.form['username']
+    password = request.form['password']
+    full_name = request.form['full_name']
+    db.add_user(username, password, full_name)
+    res = db.select_user_info(username, 'users') 
+    db.close()
+    return render_template('added.html', user_info=res)
+
+@app.route('/admin/edit/delete', methods=['POST'])
+def delete():
+    db.connect()
+    username = request.form['username']    
+    username = username.strip()
+    db.delete_user(username)
+    db.close()    
+    return render_template('deleteSuccessful.html',username= username)
+
+@app.route('/admin/delete', methods=['POST'])
+def main_delete():
+    db.connect()
+    username = request.form['username']    
+    username = username.strip()
+    db.delete_user(username)
+    return render_template('deleteSuccessful.html',username= username)
